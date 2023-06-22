2957ca309ff2fb4b6c54ed88fe8ef5c2a743ba71
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2023-06-22 14:55:04 +0200

    Update Negro.py

--- pages/Negro.py
+++ pages/Negro.py
@@ -56,14 +56,7 @@ if pokemon != '':
                 tipos = bus.get_tipo(pokemon)
                 if len(tipos) == 1:
                     path = fr"web_app/tipos/{tipos[0]}.png"
-                    st.write(path)
-                    img = open(path)
-                    content3 = img.read()
-                    data_url3 = base64.b64encode(content3).decode("utf-8")
-                    img.close()
-                    st.markdown(
-                        f'<img src="data:image/png;base64,{data_url3}" >',
-                        unsafe_allow_html=True)
+                    st.image(path)
                 
                     
                 st.write()
