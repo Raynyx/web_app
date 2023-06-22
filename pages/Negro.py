a8380cf1b2591819fd50800608ad8f27b23a0b72
GitApm <125658445+GitApm@users.noreply.github.com>
2023-06-22 17:47:21 +0200

    Update Negro.py

--- pages/Negro.py
+++ pages/Negro.py
@@ -91,7 +91,7 @@ if do:
                 contents4 = file_.read()
                 data_url4 = base64.b64encode(contents4).decode("utf-8")
                 file_.close()
-                st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width="275" height="275" alt="cat gif">',
+                st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width="400" height="400" alt="cat gif">',
                                 unsafe_allow_html=True)
             
 
