3f21ed18d7e3e7059a32c5ce4211d6bc201935a2
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2023-06-22 14:27:44 +0200

    Update buscar.py

--- buscar.py
+++ buscar.py
@@ -166,3 +166,14 @@ def get_audio(pokemon):
     url = f'https://play.pokemonshowdown.com/audio/cries/{pokemon}.mp3'
     resp = r.get(url, allow_redirects=True)
     open('crie.mp3', 'wb').write(resp.content)
+
+def get_tipo(pokemon):
+    tipos = []
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+    resp = r.get(url)
+    result = json.loads(resp.content)
+    res_tipos = result["types"]
+    tipos.append(res_tipos[0]["type"]["name"])
+    if len(res_tipos) == 2:
+        tipos.append(res_tipos[1]["type"]["name"])
+    return tipos
