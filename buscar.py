58692f9f8625d7e53b5d404bf9c1c5254a99efb3
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2023-06-22 15:28:31 +0200

    Update buscar.py

--- buscar.py
+++ buscar.py
@@ -182,6 +182,8 @@ def pinta_ruta(route, juego):
                 
     iio.mimwrite(uri="pruebaGIF_pop.gif",ims=images,loop=0, duration = 1000)
 
+    return formateado
+
 def get_sprite(pokemon):
     url = f"https://play.pokemonshowdown.com/sprites/xyani/{pokemon}.gif"
     response = r.get(url)
