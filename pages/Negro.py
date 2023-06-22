490826293ff8a67dd4315e35e6f7f2e594f4ebf3
GitApm <125658445+GitApm@users.noreply.github.com>
2023-06-22 19:28:07 +0200

    Update Negro.py

--- pages/Negro.py
+++ pages/Negro.py
@@ -90,7 +90,7 @@ if do:
                 contents4 = file_.read()
                 data_url4 = base64.b64encode(contents4).decode("utf-8")
                 file_.close()
-                st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width=700" height="700" alt="gif">',
+                st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width="600" height="600" alt="gif">',
                                         unsafe_allow_html=True)
             with st.expander("Pokemon info"):
                 st.markdown(f"**:black[{select}]**")
