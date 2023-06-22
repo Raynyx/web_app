a5d34ea3660702b6154cef801ed3a37328e6f89d
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2023-06-22 12:37:34 +0200

    Update NyB.py

--- pages/NyB.py
+++ pages/NyB.py
@@ -54,7 +54,7 @@ if pokemon != '':
                 file_.close()
                 st.markdown(f'<img src="data:image/gif;base64,{data_url2}"  width="100" height="100" alt="cat gif">',
                             unsafe_allow_html=True)
-            with 12:
+            with 1:
                 bus.get_audio(pokemon)
                 audio_file = open('crie.mp3', 'rb')
                 audio_bytes = audio_file.read()
