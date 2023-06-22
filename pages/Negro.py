e4ba5b23efd3daf3b13206c91b81a08e7b5bb8cb
GitApm <125658445+GitApm@users.noreply.github.com>
2023-06-22 19:10:21 +0200

    Update Negro.py

--- pages/Negro.py
+++ pages/Negro.py
@@ -83,18 +83,20 @@ if do:
         formateado = bus.pinta_ruta(select, "black")
         st.write(formateado)
         with modal.container():
-            file_ = open("pruebaGIF_pop.gif", "rb")
-            contents4 = file_.read()
-            data_url4 = base64.b64encode(contents4).decode("utf-8")
-            file_.close()
-            st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width="275" height="275" alt="cat gif">',
-                                unsafe_allow_html=True)
-            with st.expander("Pokemon info"):
-                st.markdown(f"**:black[{select}]**")
-                st.markdown(f":black[Nivel mínimo de la ruta: {minlevel[index]}]")
-                st.markdown(f":black[Nivel máximo de la ruta: {maxlevel[index]}]")
-                st.markdown(f":black[Forma de captura: {method[index]}]")
-                st.markdown(f":black[Chance: {chance[index]}%]")
+            c1,c2=st.columns(2)
+            with c1:
+                file_ = open("pruebaGIF_pop.gif", "rb")
+                contents4 = file_.read()
+                data_url4 = base64.b64encode(contents4).decode("utf-8")
+                file_.close()
+                st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width="275" height="275" alt="cat gif">',
+                                    unsafe_allow_html=True)
+                with st.expander("Pokemon info"):
+                    st.markdown(f"**:black[{select}]**")
+                    st.markdown(f":black[Nivel mínimo de la ruta: {minlevel[index]}]")
+                    st.markdown(f":black[Nivel máximo de la ruta: {maxlevel[index]}]")
+                    st.markdown(f":black[Forma de captura: {method[index]}]")
+                    st.markdown(f":black[Chance: {chance[index]}%]")
 
 hide_pages(['Red'])
 hide_pages(['Blue'])
