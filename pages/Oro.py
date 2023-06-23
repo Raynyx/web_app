2434f4ac154e1c6e2160df9614e9513f8dc54681
Raynyx <125463079+Raynyx@users.noreply.github.com>
2023-06-23 09:05:59 +0200

    Update Oro.py

--- pages/Oro.py
+++ pages/Oro.py
@@ -28,12 +28,12 @@ do = False
 
 if pokemon != '':
     try :
-        if bus.maps(pokemon, 'red') == 'not found':
+        if bus.maps(pokemon, 'oro') == 'not found':
             st.error("Este pokemon no se encuentra en esta generación")
         else:
-            bus.maps(pokemon, 'red')
+            bus.maps(pokemon, 'oro')
             do = True
-            res_busqueda, minlevel, maxlevel, method, chance = bus.locations(pokemon, 'red')
+            res_busqueda, minlevel, maxlevel, method, chance = bus.locations(pokemon, 'oro')
             st.success("Encontrado")
             file_ = open("pruebaGIF.gif", "rb")
             contents1 = file_.read()
@@ -53,6 +53,12 @@ if pokemon != '':
                 st.markdown(f'<img src="data:image/gif;base64,{data_url2}"  width="100" height="100" alt="cat gif">',
                             unsafe_allow_html=True)
 
+                st.write("")
+                tipos = bus.get_tipo(pokemon)
+                for tipo in tipos:
+                    path = f"tipos/{tipo}.png"
+                    st.image(path)
+
                 bus.get_audio(pokemon)
                 audio_file = open('crie.mp3', 'rb')
                 audio_bytes = audio_file.read()
@@ -67,12 +73,25 @@ if do:
         ok = st.button("BUSCAR")
     if select != 'Selecciona' and ok:
         index =  res_busqueda.index(select)
-        modal = Modal(key="Demo Key", title=select)
+        modal = Modal(key="Demo Key", title='',max_width='1000px')
+        formateado = bus.pinta_ruta(select, "oro")
+        st.write(formateado)
         with modal.container():
-            st.markdown(f"Nivel mínimo de la ruta: {minlevel[index]}")
-            st.markdown(f"Nivel máximo de la ruta: {maxlevel[index]}")
-            st.markdown(f"Forma de captura: {method[index]}")
-            st.markdown(f"Chance: {chance[index]}%")
+            x,y,z,x1,y1,z1=st.columns(6)
+            with y:
+                file_ = open("pruebaGIF_pop.gif", "rb")
+                contents4 = file_.read()
+                data_url4 = base64.b64encode(contents4).decode("utf-8")
+                file_.close()
+                st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width="450" height="400" alt="gif">',
+                                                unsafe_allow_html=True)
+
+            with st.expander("Pokemon info"):
+                    st.markdown(f"**:black[{select}]**")
+                    st.markdown(f":black[Nivel mínimo de la ruta: {minlevel[index]}]")
+                    st.markdown(f":black[Nivel máximo de la ruta: {maxlevel[index]}]")
+                    st.markdown(f":black[Forma de captura: {method[index]}]")
+                    st.markdown(f":black[Chance: {chance[index]}%]")
 
 hide_pages(['Red'])
 hide_pages(['Blue'])
