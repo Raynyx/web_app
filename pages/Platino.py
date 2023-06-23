54816c78a20e5f285f4e7f6d948136ee61e5653f
Raynyx <125463079+Raynyx@users.noreply.github.com>
2023-06-23 08:50:38 +0200

    Update Platino.py

--- pages/Platino.py
+++ pages/Platino.py
@@ -19,7 +19,7 @@ a,b,c,d,e,f,g,h,i,j = st.columns(10)
 
 with  b:
     st.image(
-           "https://2.bp.blogspot.com/_AOu2rTRHwaI/S5xwpEKLkzI/AAAAAAAAAh0/qVvmt5ob0JU/w1200-h630-p-k-no-nu/POKEMON+PLATINO.png",
+           "https://www.nintenderos.com/wp-content/uploads/2019/09/H2x1_NDS_PokemonPlatinum_esES_image1600w-e1568403709829.jpg",
             width=550)
 
 pokemon = st.text_input("Introduce el pokemon que quieres buscar:")
@@ -52,7 +52,12 @@ if pokemon != '':
                 file_.close()
                 st.markdown(f'<img src="data:image/gif;base64,{data_url2}"  width="100" height="100" alt="cat gif">',
                             unsafe_allow_html=True)
-
+                st.write("")
+                tipos = bus.get_tipo(pokemon)
+                for tipo in tipos:
+                    path = f"tipos/{tipo}.png"
+                    st.image(path)
+                    
                 bus.get_audio(pokemon)
                 audio_file = open('crie.mp3', 'rb')
                 audio_bytes = audio_file.read()
@@ -67,12 +72,26 @@ if do:
         ok = st.button("BUSCAR")
     if select != 'Selecciona' and ok:
         index =  res_busqueda.index(select)
-        modal = Modal(key="Demo Key", title=select)
+        modal = Modal(key="Demo Key", title='',max_width='1000px')
+        formateado = bus.pinta_ruta(select, "platinum")
+        st.write(formateado)
         with modal.container():
-            st.markdown(f"Nivel mínimo de la ruta: {minlevel[index]}")
-            st.markdown(f"Nivel máximo de la ruta: {maxlevel[index]}")
-            st.markdown(f"Forma de captura: {method[index]}")
-            st.markdown(f"Chance: {chance[index]}%")
+            x,y,z,x1,y1,z1=st.columns(6)
+            with y:
+                file_ = open("pruebaGIF_pop.gif", "rb")
+                contents4 = file_.read()
+                data_url4 = base64.b64encode(contents4).decode("utf-8")
+                file_.close()
+                st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width="450" height="400" alt="gif">',
+                                                unsafe_allow_html=True)
+
+            with st.expander("Pokemon info"):
+                    st.markdown(f"**:black[{select}]**")
+                    st.markdown(f":black[Nivel mínimo de la ruta: {minlevel[index]}]")
+                    st.markdown(f":black[Nivel máximo de la ruta: {maxlevel[index]}]")
+                    st.markdown(f":black[Forma de captura: {method[index]}]")
+                    st.markdown(f":black[Chance: {chance[index]}%]")
+
 
 hide_pages(['Red'])
 hide_pages(['Blue'])
