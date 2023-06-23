abb949a229d58ac850b227c85de1dfac2a56c03d
Raynyx <125463079+Raynyx@users.noreply.github.com>
2023-06-23 10:25:23 +0200

    Update Rubi.py

--- pages/Rubi.py
+++ pages/Rubi.py
@@ -34,6 +34,7 @@ if pokemon != '':
             do = True
             res_busqueda, minlevel, maxlevel, method, chance = bus.locations(pokemon, 'ruby')
             st.success("Encontrado")
+            st.image("poly.png")
             file_ = open("pruebaGIF.gif", "rb")
             contents1 = file_.read()
             data_url1 = base64.b64encode(contents1).decode("utf-8")
