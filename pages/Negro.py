f582ba79d5e563118686a25b101625430310a1ec
Raynyx <125463079+Raynyx@users.noreply.github.com>
2023-06-23 08:28:25 +0200

    Update Negro.py

--- pages/Negro.py
+++ pages/Negro.py
@@ -83,24 +83,21 @@ if do:
         formateado = bus.pinta_ruta(select, "black")
         st.write(formateado)
         with modal.container():
-            w,x,y,z=st.columns(4)
-            with w:
+            x,y,z=st.columns(4)
+            with x:
                 file_ = open("pruebaGIF_pop.gif", "rb")
                 contents4 = file_.read()
                 data_url4 = base64.b64encode(contents4).decode("utf-8")
                 file_.close()
                 st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width="400" height="400" alt="gif">',
                                                 unsafe_allow_html=True)
-            with y:
-                st.write("")
-                st.write("")
-                st.write("")
-                with st.expander("Pokemon info",expanded=True):
-                        st.markdown(f"**:black[{select}]**")
-                        st.markdown(f":black[Nivel mínimo de la ruta: {minlevel[index]}]")
-                        st.markdown(f":black[Nivel máximo de la ruta: {maxlevel[index]}]")
-                        st.markdown(f":black[Forma de captura: {method[index]}]")
-                        st.markdown(f":black[Chance: {chance[index]}%]")
+
+            with st.expander("Pokemon info",expanded=True):
+                    st.markdown(f"**:black[{select}]**")
+                    st.markdown(f":black[Nivel mínimo de la ruta: {minlevel[index]}]")
+                    st.markdown(f":black[Nivel máximo de la ruta: {maxlevel[index]}]")
+                    st.markdown(f":black[Forma de captura: {method[index]}]")
+                    st.markdown(f":black[Chance: {chance[index]}%]")
 
 hide_pages(['Red'])
 hide_pages(['Blue'])
