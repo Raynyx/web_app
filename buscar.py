c8d3f22982c133298ba96b45644a4f2d38b62bfc
Raynyx <125463079+Raynyx@users.noreply.github.com>
2023-06-23 11:22:09 +0200

    Update buscar.py

--- buscar.py
+++ buscar.py
@@ -204,8 +204,18 @@ def pinta_ruta(route, juego):
     for filename in [f"{region}.png","poly_pop.png"]:
         images.append(iio.imread(filename))
                 
+    if region != "hoenn":
+        iio.mimwrite(uri="pruebaGIF_pop.gif",ims=images,loop=0, duration = 1000)
+    else:
+        frames_pop = []
+        for i in [f"{region}.png","poly_pop.png"]:
+            new_frame_pop = Image.open(i)
+            frames_pop.append(new_frame_pop)
 
-    iio.mimwrite(uri="pruebaGIF_pop.gif",ims=images,loop=0, duration = 1000)
+        frames_pop[0].save('png_to_gif_pop.gif', format='GIF',
+                   append_images=frames_pop[1:],
+                   save_all=True,
+                   duration=300, loop=0)
 
     return formateado
 
