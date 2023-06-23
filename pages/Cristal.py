55536006a744ff3355a56483ab29881f5be74cf4
Raynyx <125463079+Raynyx@users.noreply.github.com>
2023-06-23 09:11:15 +0200

    Update Cristal.py

--- pages/Cristal.py
+++ pages/Cristal.py
@@ -21,18 +21,86 @@ with  b:
 
 pokemon = st.text_input("Introduce el pokemon que quieres buscar:")
 
-if pokemon == 'Y':
-    st.success("Hola")
-else:
-    st.error("Adios")
+do = False
 
-hide_pages(['RyB'])
+if pokemon != '':
+    try :
+        if bus.maps(pokemon, 'cristal') == 'not found':
+            st.error("Este pokemon no se encuentra en esta generación")
+        else:
+            bus.maps(pokemon, 'cristal')
+            do = True
+            res_busqueda, minlevel, maxlevel, method, chance = bus.locations(pokemon, 'cristal')
+            st.success("Encontrado")
+            file_ = open("pruebaGIF.gif", "rb")
+            contents1 = file_.read()
+            data_url1 = base64.b64encode(contents1).decode("utf-8")
+            file_.close()
+            r,l,l2,l3 = st.columns(4)
+            with r:
+                st.markdown(
+                        f'<img src="data:image/gif;base64,{data_url1}"  width="500" height="500" alt="cat gif">',
+                        unsafe_allow_html=True)
+            with l3:
+                bus.get_sprite(pokemon)
+                file_ = open("spriteGIF.gif", "rb")
+                contents2 = file_.read()
+                data_url2 = base64.b64encode(contents2).decode("utf-8")
+                file_.close()
+                st.markdown(f'<img src="data:image/gif;base64,{data_url2}"  width="100" height="100" alt="cat gif">',
+                            unsafe_allow_html=True)
+
+                st.write("")
+                tipos = bus.get_tipo(pokemon)
+                for tipo in tipos:
+                    path = f"tipos/{tipo}.png"
+                    st.image(path)
+
+                bus.get_audio(pokemon)
+                audio_file = open('crie.mp3', 'rb')
+                audio_bytes = audio_file.read()
+                
+                st.audio(audio_bytes, format='audio/mp3')
+    except:
+        st.error("Este pokemon no existe")
+        
+if do: 
+    with l3:
+        select = st.selectbox("Introduce ruta",['Selecciona'] + res_busqueda)
+        ok = st.button("BUSCAR")
+    if select != 'Selecciona' and ok:
+        index =  res_busqueda.index(select)
+        modal = Modal(key="Demo Key", title='',max_width='1000px')
+        formateado = bus.pinta_ruta(select, "cristal")
+        st.write(formateado)
+        with modal.container():
+            x,y,z,x1,y1,z1=st.columns(6)
+            with y:
+                file_ = open("pruebaGIF_pop.gif", "rb")
+                contents4 = file_.read()
+                data_url4 = base64.b64encode(contents4).decode("utf-8")
+                file_.close()
+                st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width="450" height="400" alt="gif">',
+                                                unsafe_allow_html=True)
+
+            with st.expander("Pokemon info"):
+                    st.markdown(f"**:black[{select}]**")
+                    st.markdown(f":black[Nivel mínimo de la ruta: {minlevel[index]}]")
+                    st.markdown(f":black[Nivel máximo de la ruta: {maxlevel[index]}]")
+                    st.markdown(f":black[Forma de captura: {method[index]}]")
+                    st.markdown(f":black[Chance: {chance[index]}%]")
+
+hide_pages(['Red'])
+hide_pages(['Blue'])
 hide_pages(['Yellow'])
-hide_pages(['OyP'])
+hide_pages(['Oro'])
+hide_pages(['Plata'])
 hide_pages(['Cristal'])
-hide_pages(['RyZ'])
+hide_pages(['Rubi'])
+hide_pages(['Zafiro'])
 hide_pages(['Esmera'])
-hide_pages(['DyP'])
+hide_pages(['Diamante'])
+hide_pages(['Perla'])
 hide_pages(['Platino'])
-hide_pages(['NyB'])
-hide_pages(['NyB2'])
+hide_pages(['Negro'])
+hide_pages(['Blanco'])
