c498688f417dab48f00ec1fcccf658055d6b090e
Raynyx <125463079+Raynyx@users.noreply.github.com>
2023-06-25 19:07:22 +0200

    Update Yellow.py

--- pages/Yellow.py
+++ pages/Yellow.py
@@ -15,12 +15,10 @@ from streamlit_modal import Modal
 
 st.set_page_config(
     page_title="Pokemon Yellow",
-    page_icon="🔅",
-    layout="wide",
-    initial_sidebar_state="expanded",
+    page_icon="🔅"
 )
 
-st.markdown("<h1 style='text-align: center; color: grey;'>Pokemon Amarillo</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: grey;'>Pokemon Yellow</h1>", unsafe_allow_html=True)
 
 a,b,c,d,e,f,g,h,i,j = st.columns(10) 
 
@@ -29,19 +27,19 @@ with  b:
            "https://upload.wikimedia.org/wikipedia/commons/d/d0/Pok%C3%A9mon_Yellow_Logo.png",
             width=550)
 
-pokemon = st.text_input("Introduce el pokemon que quieres buscar:")
+pokemon = st.text_input("Introduce the Pokemon you want to search:")
 
 do = False
 
 if pokemon != '':
     try :
         if bus.maps(pokemon, 'yellow') == 'not found':
-            st.error("Este pokemon no se encuentra en esta generación")
+            st.error("This pokemon can not be found in this generation")
         else:
             bus.maps(pokemon, 'yellow')
             do = True
             res_busqueda, minlevel, maxlevel, method, chance = bus.locations(pokemon, 'yellow')
-            st.success("Encontrado")
+            st.success("Found")
             file_ = open("pruebaGIF.gif", "rb")
             contents1 = file_.read()
             data_url1 = base64.b64encode(contents1).decode("utf-8")
@@ -72,17 +70,16 @@ if pokemon != '':
                 
                 st.audio(audio_bytes, format='audio/mp3')
     except:
-        st.error("Este pokemon no existe")
+        st.error("This Pokemon do not exist")
         
 if do: 
     with l3:
-        select = st.selectbox("Introduce ruta",['Selecciona'] + res_busqueda)
-        ok = st.button("BUSCAR")
-    if select != 'Selecciona' and ok:
+        select = st.selectbox("Introduce route",['Select'] + res_busqueda)
+        ok = st.button("SEARCH")
+    if select != 'Select' and ok:
         index =  res_busqueda.index(select)
         modal = Modal(key="Demo Key", title='',max_width='1000px')
         formateado = bus.pinta_ruta(select, "yellow")
-        st.write(formateado)
         with modal.container():
             x,y,z,x1,y1,z1=st.columns(6)
             with y:
@@ -95,9 +92,9 @@ if do:
 
             with st.expander("Pokemon info"):
                     st.markdown(f"**:black[{select}]**")
-                    st.markdown(f":black[Nivel mínimo de la ruta: {minlevel[index]}]")
-                    st.markdown(f":black[Nivel máximo de la ruta: {maxlevel[index]}]")
-                    st.markdown(f":black[Forma de captura: {method[index]}]")
+                    st.markdown(f":black[Minime level for this route: {minlevel[index]}]")
+                    st.markdown(f":black[Maximum level for this route: {maxlevel[index]}]")
+                    st.markdown(f":black[Way of capture: {method[index]}]")
                     st.markdown(f":black[Chance: {chance[index]}%]")
 
 hide_pages(['Red'])
