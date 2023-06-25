0509e9d99f7c8f2d81984e0d9a92e8a0f4bf901f
Raynyx <125463079+Raynyx@users.noreply.github.com>
2023-06-25 17:59:22 +0200

    Update Negro.py

--- pages/Negro.py
+++ pages/Negro.py
@@ -13,6 +13,13 @@ from streamlit_extras.switch_page_button import switch_page
 import buscar as bus
 from streamlit_modal import Modal
 
+st.set_page_config(
+    page_title="Pokemon Black",
+    page_icon="🔅",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
 st.markdown("<h1 style='text-align: center; color: grey;'>Pokemon Negro</h1>", unsafe_allow_html=True)
 
 a,b,c,d,e,f,g,h,i,j = st.columns(10) 
