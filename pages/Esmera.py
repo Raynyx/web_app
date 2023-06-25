37dd06e7939b706f32addf135b5046d2b47c8baf
Raynyx <125463079+Raynyx@users.noreply.github.com>
2023-06-25 19:04:19 +0200

    Update Esmera.py

--- pages/Esmera.py
+++ pages/Esmera.py
@@ -84,7 +84,6 @@ if do:
         index =  res_busqueda.index(select)
         modal = Modal(key="Demo Key", title='',max_width='1000px')
         formateado = bus.pinta_ruta(select, "emerald")
-        st.write(formateado)
         with modal.container():
             x,y,z,x1,y1,z1=st.columns(6)
             with y:
