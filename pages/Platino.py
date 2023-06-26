66d037d5e024254c1cf0577b6f7554de10918fd8
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2023-06-26 14:19:51 +0200

    Update Platino.py

--- pages/Platino.py
+++ pages/Platino.py
@@ -58,8 +58,8 @@ if pokemon != '':
                 data_url2 = base64.b64encode(contents2).decode("utf-8")
                 file_.close()
                 dim = Image.open("spriteGIF.gif").size
-                h = dim[0] * 1.2
-                w = dim[1] * 1.2
+                w = dim[0] * 1.3
+                h = dim[1] * 1.3
                 st.write(h)
                 st.markdown(f'<img src="data:image/gif;base64,{data_url2}"  width={w} height={h} alt="cat gif">',
                             unsafe_allow_html=True)
